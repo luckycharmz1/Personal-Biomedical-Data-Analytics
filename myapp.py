9e267ff6437b231caaf4fe708b6acf882f8d2dcf
Charmaine Washington <cwashington2011@outlook.com>
2026-01-26 21:09:48 -0800

    uploaded data

--- myapp.py
+++ myapp.py
@@ -1,2 +1,11 @@
 import streamlit as st
 import pandas as pd
+
+# My first app
+st.write("Charmaine's Data")
+
+# Read the data
+df = pd.read_csv("/Samsung Health")
+
+# Display the line chart
+st.line_chart(df)
